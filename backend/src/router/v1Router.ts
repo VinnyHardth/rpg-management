1208a2b18ecfc7ad5e77c43a79dd5a60c121020b
VinnyHardth <vinicius.fonseca@icomp.ufam.edu.br>
2024-06-07 22:23:59 -0400

    adding endpoint to api router

--- backend/src/router/v1Router.ts
+++ backend/src/router/v1Router.ts
@@ -3,11 +3,13 @@ import userRouter from "../resources/user/user.router";
 import statusRouter from "../resources/status/status.router";
 import campaignRouter from "../resources/campaign/campaign.router";
 import rpgSystemRouter from "../resources/rpgSystem/rpgSystem.router";
+import userInCampaignRouter from "../resources/userInCampaign/userInCampaign.router";
 import statusFromRpgSystemRouter from "../resources/statusFromRpgSystem/statusFromRpgSystem.router";
 
 const router = Router();
 
 router.use("/user", userRouter);
+router.use("/userInCampaign", userInCampaignRouter);
 
 router.use("/status", statusRouter);
 router.use("/campaign", campaignRouter);
