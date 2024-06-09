81670feebd9a9490279e6b19094593c2e2daeca1
VinnyHardth <vinicius.fonseca@icomp.ufam.edu.br>
2024-06-09 11:27:53 -0400

    adding new env variable to validateEnv

--- backend/src/utils/validateEnv.ts
+++ backend/src/utils/validateEnv.ts
@@ -1,10 +1,11 @@
-import { cleanEnv, port, str } from "envalid";
+import { cleanEnv, num, port, str } from "envalid";
 
 const validateEnv = () => {
   cleanEnv(process.env, {
     PORT: port(),
     DATABASE_URL: str(),
     DEFAULT_LANG: str({ choices: ["pt-BR"] }),
+    BCRYPT_ROUNDS: num({ default: 10 }),
   });
 };
 
