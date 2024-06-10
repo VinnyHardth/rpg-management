0382f7f7a95f9e215f9c351be3cbc9ced18f5ea1
VinnyHardth <vinicius.fonseca@icomp.ufam.edu.br>
2024-06-10 18:03:13 -0400

    improving endpoint generation script

--- backend/src/resources/create_resource.py
+++ backend/src/resources/create_resource.py
@@ -8,13 +8,29 @@ def criar_estrutura(nome_pasta):
     # Lista dos arquivos a serem criados
     arquivos = ['controller', 'router', 'services', 'types']
 
-    # Loop para criar os arquivos dentro da pasta
     for arquivo in arquivos:
         nome_arquivo = f"{nome_pasta}/{nome_pasta}.{arquivo}.ts"
         with open(nome_arquivo, 'w') as f:
             # Escreve algum conteúdo básico no arquivo
             f.write(f'// Este é o arquivo {nome_arquivo}\n')
             f.write('// Insira seu código aqui\n')
+
+            # Adiciona código específico para controller e router
+            if arquivo == 'controller':
+                f.write("\nimport { Request, Response } from 'express';\n")
+                f.write("import { ReasonPhrases, StatusCodes } from 'http-status-codes';\n\n")
+                f.write("const create = async (req: Request, res: Response) => {};\n")
+                f.write("const read = async (req: Request, res: Response) => {};\n")
+                f.write("const update = async (req: Request, res: Response) => {};\n")
+                f.write("const remove = async (req: Request, res: Response) => {};\n")
+            elif arquivo == 'router':
+                f.write("\nimport { Router } from 'express';\n\n")
+                f.write("const router = Router();\n\n")
+                f.write("router.post('/', create);\n")
+                f.write("router.get('/', read);\n")
+                f.write("router.put('/', update);\n")
+                f.write("router.delete('/', remove);\n\n")
+                f.write("export default router;\n")
 
     print(f'Estrutura criada com sucesso em {nome_pasta}!')
 
